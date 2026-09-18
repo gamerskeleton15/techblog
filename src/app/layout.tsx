110822ef695e732957de52fe397304746bbe9aa8
Abdul Ahad Ejaz <gamerskeleton15@gmail.com>
2026-09-18 15:38:35 +0500

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,12 @@ export default function RootLayout({
     <html lang="en" className={`${inter.variable} ${syne.variable} ${inter.className}`}>
       <head>
         {ADSENSE_CLIENT && (
-          <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3517858716332831"
-            crossOrigin="anonymous"><script/>
+          <Script
+            async
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE_CLIENT}`}
+            crossOrigin="anonymous"
+            strategy="beforeInteractive"
+          />
         )}
       </head>
       <body>
