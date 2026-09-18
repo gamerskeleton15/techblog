dbf97569e4005f3c3326be11516086db23f6cf59
Abdul Ahad Ejaz <gamerskeleton15@gmail.com>
2026-09-18 15:21:07 +0500

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,32 +30,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${syne.variable} ${inter.className}`}>
-      <body>
-        <NavbarContainer />
-        <main className="min-h-screen pb-16">{children}</main>
-        <Footer />
+      <head>
         {ADSENSE_CLIENT && (
           <Script
             async
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE_CLIENT}`}
             crossOrigin="anonymous"
-            strategy="afterInteractive"
+            strategy="beforeInteractive"
           />
         )}
-        <Script
-          id="monetag-in-page-push"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(s){s.dataset.zone='11830257',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`,
-          }}
-        />
-        <Script
-          id="monetag-zone-282471"
-          src="https://quge5.com/88/tag.min.js"
-          data-zone="282471"
-          data-cfasync="false"
-          strategy="afterInteractive"
-        />
+      </head>
+      <body>
+        <NavbarContainer />
+        <main className="min-h-screen pb-16">{children}</main>
+        <Footer />
       </body>
     </html>
   );
